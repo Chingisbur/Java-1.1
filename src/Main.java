c8f368585f32971e8125d0d7a18505dc6188531a
Chingis <chingisbur@gmail.com>
2023-09-12 19:35:18 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int prise = 13676;
+        int price = 13676;
 
         int bonus = prise / 20;
         System.out.println("Бонус : " + bonus);
