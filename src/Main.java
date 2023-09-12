c81bdedef71cefed750006585f3cec7565270b82
Chingis <chingisbur@gmail.com>
2023-09-12 10:12:27 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int prise = 13676;
+
+        int bonus = prise / 20;
+        System.out.println("Бонус : " + bonus);
+    }
+}
